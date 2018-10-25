2bbccabba0bbcb5b29f83fe1b8ffad2c2865d7f2
tharanamayu <tharana.maya@gmail.com>
2018-10-25 22:15:56 +0530

    updated add.handlebars

--- app.js
+++ app.js
@@ -37,6 +37,10 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about');
 });
+//Add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
 
 const port = 5000;
 
