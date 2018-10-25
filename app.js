83a8d8cc5c1e697c9da0ce289179dcb71d2e5cde
tharanamayu <tharana.maya@gmail.com>
2018-10-25 12:12:12 +0530

    updated app.js

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ const express =require('express');
 
 const app = express();
 
+
+//How middleware works
+app.use(function(req,res,next){
+  console.log(Date.now());
+ req.name='Tharana Mayuranga';
+  next();
+});
+
 //Index Route
 
 app.get('/',(req,res)=>{
+  console.log(req.name);
   res.send('INDEX');
 });
+
+
 
 //About Route
 
 app.get('/about',(req,res)=>{
   res.send('About');
-})
+});
 
 const port =5000;
 
