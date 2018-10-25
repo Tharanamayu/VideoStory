fa8c3665a94fe417bc255ae2ad971953010fce78
tharanamayu <tharana.maya@gmail.com>
2018-10-25 18:46:20 +0530

    added local mongodb connectioni

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-const express =require('express');
-var exphbs  = require('express-handlebars');
-
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
-//Handlebars Middleware
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+// Map global promise - get rid of warning
+//mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/videostory-dev',{
+  useNewUrlParser: true
+})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
-//Index Route
 
-app.get('/',(req,res)=>{
-  const title ='Huee'
-  res.render('index',{
-    title:title
+// Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
   });
 });
 
-
-
-//About Route
-
-app.get('/about',(req,res)=>{
+// About Route
+app.get('/about', (req, res) => {
   res.render('about');
 });
 
-const port =5000;
+const port = 5000;
 
-app.listen(port,()=>{
-
-console.log(`Server started on port ${port}`);
-//console.log('Server started on port '+port);
-
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
 });
