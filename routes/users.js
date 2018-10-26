90613faa19024f88f810236271be21c6d787d18f
tharanamayu <tharana.maya@gmail.com>
2018-10-26 19:36:32 +0530

    updated users.js

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,25 @@ router.get('/register',(req,res)=>{
   res.render('users/register');
 });
 
+//Register Form POST
+router.post('/register',(req,res)=>{
+  let errors=[];
+  if(req.body.password !=req.body.password2){
+    errors.push({text:'pasowrds do not match'});
+  }
+  if(req.body.password.length<4){
+    errors.push({text:'passowrds must be at least 4 charcters'});
+  }
+  if(errors.lenght>0){
+    res.render('users/register',{
+      errors:error,
+      name:req.body.name,
+      email:req.body.email,
+      password:req.body.password,
+      password2:req.body.password2
+    });
+  }else{
+    res.send()
+  }
+});
 module.exports=router;
