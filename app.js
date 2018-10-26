b32b69d1647202fd22539b1aca090fc5c7c86817
tharanamayu <tharana.maya@gmail.com>
2018-10-26 12:28:03 +0530

    updated app woith PUT

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const methodOverride = require('method-override');
 const bodyParser=require('body-parser');
 const mongoose = require('mongoose');
 
@@ -27,10 +28,13 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 //Body parser Middleware
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({extended:false}));
 
 //parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+//Method override middleware
+app.use(methodOverride('_method'));
 
 // Index Route
 app.get('/', (req, res) => {
@@ -103,6 +107,24 @@ app.post('/ideas', (req, res) => {
     })
   }
  
+});
+
+//Edit Form Process
+
+app.put('/ideas/:id',(req,res)=>{
+  Idea.findOne({
+    _id: req.params.id
+  })
+  .then(idea=>{
+    //new Values
+    idea.title=req.body.title;
+    idea.details=req.body.details;
+    
+    idea.save()
+      .then(idea=>{
+        res.redirect('/ideas');
+      })
+  });
 });
 
 const port = 5000;
